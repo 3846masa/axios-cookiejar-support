5c0a8fe9a8bc494b21cec1fe83104b45ba571200
3846masa <3846masahiro+git@gmail.com>
2022-12-26 16:30:40 +0900

    fix: allow to reuse config (e.g. axios-retry)
    * closes https://github.com/3846masa/axios-cookiejar-support/issues/602
    * closes https://github.com/3846masa/axios-cookiejar-support/pull/609

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import type { AxiosInstance, AxiosRequestConfig, AxiosStatic } from 'axios';
 import { HttpCookieAgent, HttpsCookieAgent } from 'http-cookie-agent/http';
 import type { CookieJar } from 'tough-cookie';
 
+const AGENT_CREATED_BY_AXIOS_COOKIEJAR_SUPPORT = Symbol('AGENT_CREATED_BY_AXIOS_COOKIEJAR_SUPPORT');
+
 declare module 'axios' {
   interface AxiosRequestConfig {
     jar?: CookieJar;
@@ -18,12 +20,28 @@ function requestInterceptor(config: AxiosRequestConfig): AxiosRequestConfig {
     throw new Error('config.jar does not accept boolean since axios-cookiejar-support@2.0.0.');
   }
 
-  if (config.httpAgent || config.httpsAgent) {
+  if (
+    (config.httpAgent != null && config.httpAgent[AGENT_CREATED_BY_AXIOS_COOKIEJAR_SUPPORT] !== true) ||
+    (config.httpsAgent != null && config.httpsAgent[AGENT_CREATED_BY_AXIOS_COOKIEJAR_SUPPORT] !== true)
+  ) {
     throw new Error('axios-cookiejar-support does not support for use with other http(s).Agent.');
   }
 
   config.httpAgent = new HttpCookieAgent({ cookies: { jar: config.jar } });
+  Object.defineProperty(config.httpAgent, AGENT_CREATED_BY_AXIOS_COOKIEJAR_SUPPORT, {
+    configurable: false,
+    enumerable: false,
+    value: true,
+    writable: false,
+  });
+
   config.httpsAgent = new HttpsCookieAgent({ cookies: { jar: config.jar } });
+  Object.defineProperty(config.httpsAgent, AGENT_CREATED_BY_AXIOS_COOKIEJAR_SUPPORT, {
+    configurable: false,
+    enumerable: false,
+    value: true,
+    writable: false,
+  });
 
   return config;
 }
