198805f7bc577bb209bd1824f62a0dfde42c786d
3846masa <3846masahiro+git@gmail.com>
2024-05-01 21:34:28 +0900

    chore: convert prettier config to mjs

--- prettier.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  ...require('@3846masa/configs/prettier'),
-};
---
+++ prettier.config.mjs
@@ -0,0 +1,5 @@
+import { config as sharedConfig } from '@3846masa/configs/prettier/config.mjs';
+
+export default {
+  ...sharedConfig,
+};
